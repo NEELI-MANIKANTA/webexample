023fe77b2a784e4cb222e5516ef4c35e72817a58
NEELI-MANIKANTA <113284432+NEELI-MANIKANTA@users.noreply.github.com>
2024-03-19 20:49:04 +0530

    adding maven project

---
+++ src/main/java/com/sathya/springboot/TestController.java
@@ -0,0 +1,19 @@
+package com.sathya.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test")
+	public String display()
+	{
+		return "data.html";
+	}
+	@RequestMapping("/student")
+	public String student()
+	{
+		return "student.html";
+	}
+}
